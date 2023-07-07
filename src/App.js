b103b0e433104fe57fd3664d526c5c6927632eb4
HyeongChank <jsk381988@gmail.com>
2023-07-07 12:43:10 +0900

    update

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 // import Dashboard from './raspberry/Dashboard';
 import Testsocket from './raspberry/Testsocket';
--- src/raspberry/Testsocket.jsx
+++ src/raspberry/Testsocket.jsx
@@ -1,18 +1,49 @@
 import React, { useState, useEffect } from 'react';
 import socketIOClient from "socket.io-client";
-
+import './Dashboardc.css'
 const Testsocket = () => {
-  const [data1, setData1] = useState(null);
-  const [data2, setData2] = useState(null);
+  const [process_time, setProcess_time] = useState()
+  // const [machineA1_status, setMachineA1_status] = useState(null);
+  // const [machineA1_part, setMachineA1_part] = useState(null);
+  const [machineA1_count, setMachineA1_count] = useState(0);
+  // const [machineA1_unnormal, setMachineA1_unnormal] = useState()
+  // const [machineA2_status, setMachineA2_status] = useState()
+  // const [machineA2_part, setMachineA2_part] = useState()
+  const [machineA2_count, setMachineA2_count] = useState(0)
+  // const [machineA2_unnormal, setMachineA2_unnormal] = useState()
+  const [machineA3_count, setMachineA3_count] = useState(0)
+  const [machineB, setMachineB] = useState(0)
+  const [machineC, setMachineC] = useState(0)
+  const [problem, setProblem] = useState(null)
 
   useEffect(() => {
     const socket = socketIOClient('http://localhost:5000/test');
-    socket.on('data1', (message) => {
-      setData1(message.product);
+
+    socket.on('process_time', (message) => {
+      setProcess_time(message.Time);
     });
-    socket.on('data2', (message) => {
-      setData2(message.product);
+    socket.on('problem', (message) => {
+      setProblem(message.problem);
     });
+
+    socket.on('machineA1_count', (message) => {
+      setMachineA1_count(message.count);
+    });
+    
+    socket.on('machineA2_count', (message) => {
+      setMachineA2_count(message.count);
+    });
+    
+    socket.on('machineA3_count', (message) => {
+      setMachineA3_count(message.count);
+    });
+    socket.on('completed_product', (message) => {
+      setMachineB(message.completed_product);
+    });
+    socket.on('unnormal_total', (message) => {
+      setMachineC(message.unnormal_total);
+    });
+
 
     // Disconnect when the client unmounts
     return () => {
@@ -21,9 +52,51 @@ const Testsocket = () => {
   }, []);
 
   return (
-    <div>
-      <h1>Data 1: {data1}</h1>
-      <h1>Data 2: {data2}</h1>
+    <div className='setposition'>
+      <h1>Dashboard</h1>
+      <p>Time : {process_time}</p>
+      <p>Operate : {problem}</p>
+      <div className='part_table'>
+        <table>
+          <thead>
+            <tr>
+              <th>Part</th>
+              <th>Count</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Head</td>
+              <td>{machineA1_count}</td>
+            </tr>
+            <tr>
+              <td>Body</td>
+              <td>{machineA2_count}</td>
+            </tr>
+            <tr>
+              <td>Foot</td>
+              <td>{machineA3_count}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      <div className='count_table'>
+        <table>
+          <thead>
+            <tr>
+              <th>Complete_Product_Count</th>
+              <th>Defective_Product_Count</th>
+            </tr>
+            
+          </thead>
+          <tbody>
+            <tr>
+              <td>{machineB}</td>
+              <td>{machineC}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
